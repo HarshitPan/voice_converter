3d2380c0d04d3c904aa3b3a9af2d1a822c684a08
harsh <harshitpandeyfeelings@gmail.com>
2023-10-22 11:30:39 +0530

    Added malyalam translator

--- translate.py
+++ translate.py
@@ -4,3 +4,7 @@ translate=Translator()
 def toHindi(text):
     global translate
     return translate.translate(text,dest="hi")
+
+def toMalyalam(text):
+    global translate
+    return translate.translate(text,dest="ml")
